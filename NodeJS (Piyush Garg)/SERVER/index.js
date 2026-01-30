94dd0d692249409bff4f1b39ad36e6e46896c153
prajwaldeulkar47 <prajwaldeulkar47@gmail.com>
2026-01-31 00:03:49 +0530

    yo

--- NodeJS (Piyush Garg)/SERVER/index.js
+++ NodeJS (Piyush Garg)/SERVER/index.js
@@ -1,21 +1,24 @@
 const http = require('http')
 const fs = require('fs')
-const myUrl = require('url')
+const url = require('url')
 
 const myServer = http.createServer((req, res) => {
     if (req.url === '/favicon.ico' || req.url === '/.well-known/appspecific/com.chrome.devtools.json') {
         return res.end()
     }
-    
+
     const log = `${Date.now()}: ${req.url} : New request received`
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl)
 
     fs.appendFile('log.txt', log + '\n', (err) => {
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/':
                 res.end('Home Page!')
                 break
             case '/about':
-                res.end('About Page!')
+                const username = myUrl.query.username
+                res.end(`Welcome ${username} to the About Page!`)
                 break
             default:
                 res.end('404 Page Not Found!')
